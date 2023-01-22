bbec4a2cd831f97bf5852c31c6ca38b53af5a0e4
Erlin Dominguez <erlind299@gmail.com>
2023-01-21 21:06:26 -0500

    Adding background, images and design of background

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
 import "./App.css";
+import Target from "./components/Target/Target";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <div className="App">
+      <Target />
+    </div>
+  );
 }
 
 export default App;
---
+++ src/components/Target/Target.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import "./Target.css";
+
+function Target() {
+  return (
+    <div className="content">
+      <div className="target">
+        <aside className="aside"></aside>
+      </div>
+    </div>
+  );
+}
+
+export default Target;
